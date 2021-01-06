4c9276322435051d7a5beeb351075dd074396147
kamimi01 <awakaru0122@gmail.com>
2021-01-06 23:12:22 +0900

    refactor(ascii.py): ハードコードをやめて、ファイル数を取得

--- ascii.py
+++ ascii.py
@@ -1,5 +1,17 @@
 import random
 import os
-i = random.randrange(1) + 1 
-website="https://raw.githubusercontent.com/kamimi01/ASCII-Art-Splash-Screen/master/art/"+ str(i) + ".txt"
+
+# txtファイルを格納しているGitレポジトリのディレクトリ
+gitRepositoryDir = "https://raw.githubusercontent.com/kamimi01/ASCII-Art-Splash-Screen/master/art/"
+
+# ファイル数をカウント
+path = "./art"
+files = os.listdir(path)
+numOfFiles = len(files)
+
+# ランダムにファイルを取得する
+i = random.randrange(numOfFiles) + 1 
+website = gitRepositoryDir + str(i) + ".txt"
+
+# curlコマンド実行
 os.system("curl " +  website)
